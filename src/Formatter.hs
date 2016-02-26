b74c7597cc3fb32055456e6747a8d69cca785f1f
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-02-26 15:05:54 -0500

    newlines don't get gobbled when getting semicolons

--- src/Formatter.hs
+++ src/Formatter.hs
@@ -1,47 +1,39 @@
 module Formatter(formatFile) where
 import Text.Regex.Posix
 import Text.Regex
+import Debug.Trace
 
 
 occupiedLineRegex =  mkRegex "[^ \t\n][^\n]*\n" 
 docStringRegex =  "(/\\*(.|\n)*\\*/)" :: String
-commentRegex =  mkRegex "//.*" :: String
+commentRegex =  mkRegex "//.*"
 removeDocs :: String -> (String, String,String)
 removeDocs fileContents =
   let matches = fileContents =~ docStringRegex :: MatchResult String
       
   in ((mrBefore matches),(mrMatch matches), (mrAfter matches))
 
-{-removeNewLines :: String -> String
-removeNewLines prog =
-    let matches = (prog =~ occupiedLineRegex :: [[String]])
-    in foldr  (\s p -> 
-        let match =  s =~ commentRegex :: MatchResult String
-            s1 = mrBefore match
-            comm = mrMatch match
-            r = if (last s1 == '\n') 
-                then init s1 
-                else s1
-        in r++";"++comm++"\n"++p) "" (concat matches)-}
+
 handleLineComments line = 
-  case matchRegexAll commentRegex line of
-    Nothing -> prog
-    Just(s1, comm,_,_)-> 
-      let r = if (last s1 == '\n') 
-              then init s1 
-              else s1
-      in r++";"++comm++"\n"
+  let (stmnt,comm) = case matchRegexAll commentRegex line of
+          Nothing -> (line,"")
+          Just(withoutComment, comm,_,_)-> (withoutComment,comm)
+      stmntWithoutNewline = if (last stmnt == '\n') 
+              then init stmnt 
+              else stmnt
+      in stmntWithoutNewline++";"++comm++"\n"
+
 removeNewLines :: String -> String
 removeNewLines prog =
   case matchRegexAll occupiedLineRegex prog of
     Nothing -> prog
-    Just(before, matched,after,_)-> before++(handleLineComments matched) ++(removeNewLines  after)
+    Just(before, matched,after,_)-> before++(handleLineComments matched) ++(removeNewLines  after) 
 
 
 formatFile contents =
     let y@(h,d,p) = removeDocs contents
-        h1 = removeNewLines h
-        p1 = removeNewLines p
+        h1 = removeNewLines h 
+        p1 = removeNewLines p 
     in   (h1 ++ d++ "\n" ++ p1)
 
 
@@ -49,7 +41,7 @@ testFormatter = putStrLn $ formatFile "\
 \File()\n\
 \/* ohai \n\
 \tutta*/\n\
-\hello!\n\
+\hello!//kites\n\
 \\n\
 \\n\
 \\n\
