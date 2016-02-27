2e6a9b8f6af4161c7a5f45135206b1f0ea2cab12
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-02-26 20:32:49 -0500

    semi before comment but not on empty

--- src/Formatter.hs
+++ src/Formatter.hs
@@ -5,10 +5,11 @@ import Text.Regex
 import Debug.Trace
 
 
-occupiedLineRegex =  mkRegex "[^ \t\n][^\n(//)]*\n" 
+occupiedLineRegex =  mkRegex "[^ \t\n][^\n]*\n" 
 docStringRegex =  "(/\\*(.|\n)*\\*/)" :: String
 commentRegex =  mkRegex "//.*"
 removeDocs :: String -> (String, String,String)
+onlyemptyRegex = "([ \t]*(\n|$))"
 removeDocs fileContents =
   let matches = fileContents =~ docStringRegex :: MatchResult String
       
@@ -16,39 +17,32 @@ removeDocs fileContents =
 
 
 handleLineComments line = 
-  let (stmnt,comm) = case matchRegexAll commentRegex line of
-          Nothing -> (line,"")
-          Just(withoutComment, comm,_,_)-> (withoutComment,comm)
-  in if stmnt =="" 
-     then "\n"
-     else let stmntWithoutNewline = if (last stmnt == '\n') 
-                                then init stmnt 
-                                else stmnt
-          in stmntWithoutNewline++";"++comm++"\n"
+  let (stmnt,comm) = case matchRegexAll commentRegex (trace "" line) of
+          Nothing ->  (line,"")
+          Just(withoutComment, comm,_,_)-> ( (withoutComment,comm))
+  in  if ((isEmpty stmnt) || null stmnt) 
+      then line
+      else  let stmntWithoutNewline = if (  (last stmnt == '\n') )
+                                     then init stmnt 
+                                     else stmnt
+            in stmntWithoutNewline++";"++comm++"\n"
+
+isEmpty line =(onlyemptyRegex=~line)::Bool
+
 
 removeNewLines :: String -> String
 removeNewLines prog =
   case matchRegexAll occupiedLineRegex prog of
-    Nothing -> prog
-    Just(before, matched,after,_)-> before++(handleLineComments matched) ++(removeNewLines  after) 
+    Nothing -> handleLineComments prog
+    Just(before, matched,after,_)-> (before)++(handleLineComments matched) ++(removeNewLines  after) 
 
 
 formatFile contents =
     let y@(h,d,p) = removeDocs contents
-        h1 = removeNewLines h 
-        p1 = removeNewLines p 
-    in   (h1 ++ d++ "\n" ++ p1)
+        h1 =  (removeNewLines h )
+        p1 =  (removeNewLines p )
+    in   (h1 ++ d ++ p1)
 
 
-testFormatter = putStrLn $ formatFile "\
-\File()\n\
-\/* ohai \n\
-\tutta*/\n\
-\hello!//kites\n\
-\\n\
-\\n\
-\\n\
-\bye\n\
-\\n\
-\kettle\n"
+testFormatter = putStrLn $ show $ formatFile "File()\n/* ohai \ntutta*/\nhello!//kites\n//might\n\n\nbye\n\nkettle"
 
